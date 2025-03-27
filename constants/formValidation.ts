de88de9e77cc485b5793d53c94a0157696c84861
Hyeongmin Jo <paengdal1@gmail.com>
2025-03-27 14:28:25 +0900

    feat: 비밀번호 유효성 검증 정규식 추가 - 최소 8자 이상, 영문/숫자/특수문자 모두 포함

--- constants/formValidation.ts
+++ constants/formValidation.ts
@@ -9,9 +9,10 @@ const email = {
 
 const password = {
   required: '필수 입력 항목입니다',
-  minLength: {
-    value: 8,
-    message: '비밀번호를 8자 이상 입력해 주세요',
+  pattern: {
+    value:
+      /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+~`\-={}[\]:;"'<>,.?/\\]).{8,}$/,
+    message: '최소 8자 이상, 영문/숫자/특수문자를 모두 포함해야 합니다',
   },
 };
 
