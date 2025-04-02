0163029b0913ead2b4ca487633d615c33bbab7bd
HJ <a01025105259@gmail.com>
2025-04-02 18:33:11 +0900

    fix: x버튼 위치 수정

--- components/molecules/GNBMenu.tsx
+++ components/molecules/GNBMenu.tsx
@@ -11,7 +11,10 @@ const GNBMenu = ({ items = [], onClose }: GNBMenuProps) => {
   return (
     <div className="fixed top-0 right-0 h-full w-64 bg-white shadow-lg z-50 transition-transform transform translate-x-0">
       <div className="p-4">
-        <CloseButton onClick={onClose} />
+        <div className="flex justify-end">
+          <CloseButton onClick={onClose} />
+        </div>
+
         <div className="mt-4 flex flex-col ">
           {items.map(({ text, to }) => (
             <div key={to} className="w-[220px] h-[74px] flex items-center">
