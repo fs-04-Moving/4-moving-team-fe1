a07bad980fd68a5726e9b79169f326d2fe4221f3
Kimhyeog <89672036+Kimhyeog@users.noreply.github.com>
2025-04-02 18:42:24 +0900

    Revert "[김형준] Feature/24"

--- components/atoms/Input.tsx
+++ components/atoms/Input.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 import LabelInput from './LabelInput';
 
-interface Props extends React.ComponentProps<"input"> {
+interface Props extends React.ComponentProps<'input'> {
   label?: string;
   bgColor?: boolean;
   errorMessage?: string;
@@ -37,21 +37,21 @@ function Input({
 }: Props) {
   // 아래 코드에는 clsx를 굳이 쓰지 않아도 되지만, 쓰지 않을 경우 tailwind 문법으로 인식을 하지 못하여 자동 완성이 안 됨
   const defaultClassName = clsx(
-    "w-[327px] lg:w-full text-Black-400 lg:text-xl h-[54px] lg:h-16 px-[14px] py-4 rounded-2xl outline-Primay-Blue-300 border border-solid border-Line-200 placeholder-GrayScale-400"
+    'w-[327px] lg:w-full text-Black-400 lg:text-xl h-[54px] lg:h-16 px-[14px] py-4 rounded-2xl outline-Primay-Blue-300 border border-solid border-Line-200 placeholder-GrayScale-400'
   );
 
   const errorClassName = clsx({
-    "outline-Secondary-Red-200 mb-2 border-solid border-2 border-Secondary-Red-200":
+    'outline-Secondary-Red-200 mb-2 border-solid border-2 border-Secondary-Red-200':
       !!errorMessage,
   });
 
   const searchClassName = clsx({
-    "pl-10 lg:pl-13": isSearchLeft,
-    "pr-21 lg:pr-28": isSearchRight,
+    'pl-10 lg:pl-13': isSearchLeft,
+    'pr-21 lg:pr-28': isSearchRight,
   });
 
   const bgClassName = clsx({
-    "bg-[#f7f7f7] !border-none": bgColor,
+    'bg-[#f7f7f7] !border-none': bgColor,
   });
 
   return (
