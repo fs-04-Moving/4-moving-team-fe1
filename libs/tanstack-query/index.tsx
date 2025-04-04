cac36b1e530c8b5d06b55d0b79921a8e27b81bea
Hyeongmin Jo <paengdal1@gmail.com>
2025-04-04 12:59:36 +0900

    fix: tanstack-query의 default staleTime을 5분으로 설정

--- libs/tanstack-query/index.tsx
+++ libs/tanstack-query/index.tsx
@@ -1,7 +1,5 @@
-// In Next.js, this file would be called: app/providers.tsx
 'use client';
 
-// Since QueryClientProvider relies on useContext under the hood, we have to put 'use client' on top
 import {
   isServer,
   QueryClient,
@@ -12,9 +10,7 @@ function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
-        staleTime: 60 * 1000, // 1분
+        staleTime: 1000 * 60 * 5, // 5분(상황에 따라 조정)
         retry: 0,
       },
     },
@@ -28,10 +24,6 @@ function getQueryClient() {
     // Server: always make a new query client
     return makeQueryClient();
   } else {
-    // Browser: make a new query client if we don't already have one
-    // This is very important, so we don't re-make a new client if React
-    // suspends during the initial render. This may not be needed if we
-    // have a suspense boundary BELOW the creation of the query client
     if (!browserQueryClient) browserQueryClient = makeQueryClient();
     return browserQueryClient;
   }
