05e90a86f6f79a3d62d4cb694173b2fe4cbee987
gitit-emkoo <kwcc2020@naver.com>
2025-04-08 15:03:46 +0900

    feat:프로그레스바 구현

--- components/molecules/ButtonStarRating.tsx
+++ components/molecules/ButtonStarRating.tsx
@@ -10,26 +10,29 @@ import EmptyStar from '@/components/atoms/IconEmptyStar';
  * @description
  * 유저가 별점을 선택하거나 기존 별점을 표시하는 컴포넌트입니다.
  * 선택된 별점에 따라 꽉찬별(FullStar)과 빈별(EmptyStar) 컴포넌트를 보여줍니다.
- * 별 사이즈를 수정할수 있습니다. width={} 이렇게 사용하면 됩니다. 
+ * 별 사이즈를 수정할수 있습니다. starSize={20} 이런식으로로 사용하면 됩니다.(픽셀) 
  * 별점을 입력하는 용도로도 사용하고(클릭가능) 받은 데이터를 표시하는 용도(클릭불가)로도 사용 가능합니다. 
  *
  * @param {object} props - 컴포넌트 props 받습니다.
  * @param {boolean} [props.disabled=false] - true이면 클릭이 비활성화되고, false이면 클릭이 활성화됩니다.
  * @param {number} [props.initialRating] - 초기 별점 값입니다. 이 값이 기준으로 별이 채워집니다.
+ * @param {number} [props.starSize=20] - 별의 크기를 설정합니다. 기본값은 20px 입니다.
  *
  * @example
  * // 클릭 가능한 별점 입력 컴포넌트
- * <StarLating disabled={false} />
+ * <ButtonStarRating disabled={false} />
  *
  * // 클릭 불가능한 별점 표시 컴포넌트 (초기 별점 3점)
- * <StarLating disabled={true} initialRating={3} />
+ * <ButtonStarRating disabled={true} initialRating={3} />
+ * 
+  * // 별 크기를 30px로 설정
+  * <ButtonStarRating starSize={30} />
  */
-
 
 const MemoizedFullStar = memo(FullStar);
 const MemoizedEmptyStar = memo(EmptyStar);
 
-function ButtonStarRating({ disabled = false, initialRating }) {
+function ButtonStarRating({ disabled = false, initialRating, starSize = 20 }) {
   const ArrayIndexes = [1, 2, 3, 4, 5];
   const [ratingIndex, setRatingIndex] = useState(5);
 
@@ -39,7 +42,7 @@ function ButtonStarRating({ disabled = false, initialRating }) {
     }
   }, [initialRating]); 
 
-  const handleClick = (arrayindex) => {
+  const handleStarClick = (arrayindex) => {
     if (!disabled) {
       setRatingIndex(arrayindex);
     }
@@ -50,10 +53,10 @@ function ButtonStarRating({ disabled = false, initialRating }) {
       {ArrayIndexes.map((arrayindex, index) => (
         <div
           key={`rating_${index}`}
-          className={`size-[20px] ${disabled ? 'cursor-default' : 'cursor-pointer'}`}
-          onClick={() => handleClick(arrayindex)}
+          className={`size-[${starSize}px] ${disabled ? 'cursor-default' : 'cursor-pointer'}`}
+          onClick={() => handleStarClick(arrayindex)}
         >
-          {arrayindex <= ratingIndex ? <MemoizedFullStar width={20} /> : <MemoizedEmptyStar width={20} />}
+          {arrayindex <= ratingIndex ? <MemoizedFullStar width={starSize} /> : <MemoizedEmptyStar width={starSize} />}
         </div>
       ))}
     </div>
