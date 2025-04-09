adfcdbfe437ccd8c7c9b04478e626ad498f6b1f0
gitit-emkoo <kwcc2020@naver.com>
2025-04-09 19:41:32 +0900

    refactor: 머지 후 타입정의

--- app/(providers)/(root)/test/kem/page.tsx
+++ app/(providers)/(root)/test/kem/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import ChartStarRatingReview from "@/components/organisms/ChartStarRatingReview";
-import ButtonStarRating from "@/components/molecules/ButtonStarRating";
 import ProgressBar from "@/components/molecules/ProgressBar";
 import ChatBubbleMovingChoice from "@/components/molecules/ChatBubbleMovingChoice";
 
@@ -11,7 +10,6 @@ function page() {
   return (
     <div className="bg-Black-300 p-4 g-6">
       <ChartStarRatingReview ratingData={exampleRatingData} />
-      <ButtonStarRating starSize={20} />
       <div className="mb-8">
         <ProgressBar totalSteps={4} currentStep={1} />
       </div>
