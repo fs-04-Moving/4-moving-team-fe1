b1e8da5cb3bcc022052e7fe85b82c40d7256b8bf
Hyeongmin Jo <paengdal1@gmail.com>
2025-04-14 15:33:00 +0900

    feat: queryClient 생성 코드 분리(임시저장)

--- components/organisms/Gnb.tsx
+++ components/organisms/Gnb.tsx
@@ -7,7 +7,7 @@ function Gnb() {
   return (
     <header className="sticky z-20 top-0 backdrop-blur-3xl border-b-1 border-GrayScale-100">
       <div
-        className="flex justify-between items-center h-22 px-6 md:px-18 lg:px-[200px]"
+        className="flex justify-between items-center h-16 lg:h-22 px-6 md:px-18 lg:px-[200px]"
         // className={`flex justify-between items-center h-22 ${
         //   !isLoggedIn
         //     ? 'px-6 md:px-8 lg:px-[120px]'
