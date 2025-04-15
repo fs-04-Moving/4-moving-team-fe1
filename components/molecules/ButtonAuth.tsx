8b305e6c26cd7f34ee7ffea66144d74795d4c196
paengdal <40021741+paengdal@users.noreply.github.com>
2025-04-15 14:28:29 +0900

    [조형민] queryClient 생성 방식 변경, '찜한 기사님' 페이지 구현, Gnb 알림/프로필 팝업 메뉴 구현 등 (#90)
    * feat: (PR #89에 이어서 구현)queryClient 생성 방식 변경
    - 기존: providers에서 서버와 클라이언트(브라우저) 분기
    - 변경: createServerQueryClient와 getBrowserQueryClient 함수 별도 분리
      - 각 함수에 기본 옵션값 설정(파라미터로 커스텀 옵션 설정 가능)
    - 현재 프로젝트 구조에서는 providers에서만 분기해도 충분하나 복잡한 프로젝트에서는 분리가 필요(학습을 위해 분리 구조 구현해 봄)
    
    * feat: '찜한 기사님 목록'페이지 구현
    - tanstack query prefetch를 이용한 SSR 구현
    - LoadingSpinner구현
    
    * feat: 카드 클릭 시 기사님 상세로 이동 링크 추가, hover/active 효과 추가
    
    * feat: Gnb에 알림, 프로필 메뉴 팝업 구현
    - 외부 영역 클릭 감지 훅 적용(useOutsideClick)
    - 기존 DropdownProfile, DropdownNotification코드 일부 수정
      - CSS수정, 외부 영역 클릭 감지 코드 삭제(주석)
      - Link태그를 router로 수정 등

--- components/molecules/ButtonAuth.tsx
+++ components/molecules/ButtonAuth.tsx
@@ -1,18 +1,29 @@
 'use client';
 
 import userApi from '@/api/user/user.api';
-import icAlarm from '@/assets/images/ic-alarm.svg';
 import icMenu from '@/assets/images/ic-menu.svg';
-import icProfile from '@/assets/images/ic-profile.svg';
 import { useAuth } from '@/contexts/AuthContext';
+import useOutsideClick from '@/hooks/useOutsideClick';
 import { getBrowserQueryClient } from '@/libs/tanstack-query/reactQueryConfig';
+import { GetUserMe } from '@/types/dtos/user.dto';
 import { useQuery } from '@tanstack/react-query';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { useCallback, useRef, useState } from 'react';
 import ButtonSolid from '../atoms/ButtonSolid';
+import IconAlarm from '../atoms/IconAlarm';
+import DropdownNotification from './DropdownNotifications';
+import DropdownProfile from './DropdownProfile';
+import UserProfile from './UserProfile';
 
 function ButtonAuth() {
   const { logOut } = useAuth();
+  const [isShowProfilePopup, setIsShowProfilePopup] = useState(false);
+  const [isShowNotificationsPopup, setIsShowNotificationsPopup] =
+    useState(false);
+
+  const popupProfileRef = useRef<HTMLDivElement | null>(null);
+  const popupNotificationRef = useRef<HTMLDivElement | null>(null);
 
   const userQueryClient = getBrowserQueryClient({
     queries: {
@@ -20,11 +31,10 @@ function ButtonAuth() {
       retry: 0,
     },
   });
-  const { data: user } = useQuery({
+  const { data: user } = useQuery<GetUserMe>({
     queryKey: ['me'],
     queryFn: userApi.getUserMe,
     initialData: () => userQueryClient.getQueryData(['me']),
-    enabled: typeof window !== 'undefined', // CSR에서만 실행되게
   });
 
   const router = useRouter();
@@ -32,47 +42,69 @@ function ButtonAuth() {
     router.push('/auth/log-in');
   };
 
-  const handleClickLogOut = () => {
+  // 자주 사용되진 않으나 Dropdown컴포넌트에 전달되어 사용되므로
+  // useCallback을 사용해서 문제될 것은 없다고 판단하여 일단 적용해 봄(추후 자세히 분석해보자)
+  const handleClickLogOut = useCallback(() => {
+    setIsShowNotificationsPopup(false);
+    setIsShowProfilePopup(false);
     logOut?.();
     userQueryClient.removeQueries({ queryKey: ['me'] });
+  }, [logOut, userQueryClient]);
+
+  const handleClickAlarm = () => {
+    setIsShowNotificationsPopup(!isShowNotificationsPopup);
+    setIsShowProfilePopup(false);
   };
+
+  const handleClickProfile = () => {
+    setIsShowProfilePopup(!isShowProfilePopup);
+    setIsShowNotificationsPopup(false);
+  };
+
+  // 외부 영역 클릭 감지 훅 적용
+  useOutsideClick(
+    popupProfileRef,
+    () => setIsShowProfilePopup(false),
+    isShowProfilePopup
+  );
+  useOutsideClick(
+    popupNotificationRef,
+    () => setIsShowNotificationsPopup(false),
+    isShowNotificationsPopup
+  );
 
   if (user) {
     return (
-      <div className="flex items-center">
-        <Image
-          src={icAlarm}
-          alt="알림"
-          className="w-6 h-6 lg:w-9 lg:h-9 cursor-pointer"
+      <div className="flex items-center relative">
+        <IconAlarm onClick={handleClickAlarm} />
+        <UserProfile
+          onClick={handleClickProfile}
+          name={user.name}
+          profileImage={user.profileImage}
         />
-
-        <div className="flex items-center relative w-6 h-6 lg:w-9 lg:h-9 ml-6 lg:ml-8 cursor-pointer">
-          {user.profileImage ? (
-            <Image
-              src={user.profileImage}
-              alt="프로필 이미지"
-              fill
-              className="rounded-full"
-            />
-          ) : (
-            <Image
-              src={icProfile}
-              alt="빈 프로필"
-              className="w-6 h-6 lg:w-9 lg:h-9"
-            />
-          )}
-        </div>
-        <p className="hidden ml-4 lg:block text-lg font-medium text-Black-400 cursor-pointer">
-          {user.name}
-        </p>
+        {isShowProfilePopup && (
+          <DropdownProfile
+            username={user.name}
+            role={user.role}
+            isOpen={isShowProfilePopup}
+            onClose={() => setIsShowProfilePopup(false)}
+            logOut={handleClickLogOut}
+            ref={popupProfileRef}
+          />
+        )}
+        {isShowNotificationsPopup && (
+          <DropdownNotification
+            isOpen={isShowNotificationsPopup}
+            notifications={[]}
+            onClose={() => setIsShowNotificationsPopup(false)}
+            ref={popupNotificationRef}
+          />
+        )}
         <Image
           src={icMenu}
           alt="메뉴 아이콘"
           className="w-6 h-6 ml-6 cursor-pointer lg:hidden"
         />
-        <p onClick={handleClickLogOut} className="ml-4 hidden lg:inline">
-          로그아웃
-        </p>
       </div>
     );
   } else {
--- components/molecules/DropdownProfile.tsx
+++ components/molecules/DropdownProfile.tsx
@@ -11,6 +11,8 @@
  *   isOpen={isOpen}
  *   onClose={() => setIsOpen(false)}
  *   role="customer"
+ *   logOut={handleLogOut}
+ *   ref={dropdownRef}
  * />
  *
  * @param {string} props.username - 사용자 이름
@@ -23,114 +25,122 @@
 
 import clsx from 'clsx';
 import Link from 'next/link';
-import { useEffect, useRef } from 'react';
+import { useRouter } from 'next/navigation';
+import { forwardRef } from 'react';
 import { Role } from '../../types/entities/user.entity';
 
 interface DropdownProfileProps {
   username: string;
   isOpen: boolean;
   onClose: () => void;
+  logOut: () => void;
   role: Role; // 기존 타입 정의 사용: 'customer' 또는 'worker'
 }
 
-function DropdownProfile({
-  username,
-  isOpen,
-  onClose,
-  role,
-}: DropdownProfileProps) {
-  const dropdownRef = useRef<HTMLDivElement>(null);
+const DropdownProfile = forwardRef<HTMLDivElement, DropdownProfileProps>(
+  function DropdownProfile({ username, isOpen, onClose, logOut, role }, ref) {
+    const router = useRouter();
 
-  // 외부 클릭 감지
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      const target = event.target as HTMLElement;
-      if (target.closest('[data-button-id="profile-button"]')) {
-        return;
-      }
-
-      if (dropdownRef.current && !dropdownRef.current.contains(target)) {
-        onClose();
-      }
+    const handleClickFavorite = () => {
+      router.push('/customer/favorites');
+      onClose();
     };
 
-    if (isOpen) {
-      setTimeout(() => {
-        document.addEventListener('mousedown', handleClickOutside);
-      }, 0);
-    }
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+    const handleClickReview = () => {
+      router.push('/customer/reviews/pending');
+      onClose();
     };
-  }, [isOpen, onClose]);
 
-  // 로그아웃 처리 일단 요기에 자리 만들었습니다
-  const handleLogout = () => {
-    console.log('로그아웃 처리');
-    onClose();
-  };
+    // 외부 클릭 감지용 useEffect를 사용할 수 있습니다.
+    // const dropdownRef = useRef<HTMLDivElement>(null);
+    // useEffect(() => {
+    //   const handleClickOutside = (event: MouseEvent) => {
+    //     const target = event.target as HTMLElement;
+    //     if (target.closest('[data-button-id="profile-button"]')) {
+    //       return;
+    //     }
+    //     if (dropdownRef.current && !dropdownRef.current.contains(target)) {
+    //       onClose();
+    //     }
+    //   };
+    //   if (isOpen) {
+    //     setTimeout(() => {
+    //       document.addEventListener('mousedown', handleClickOutside);
+    //     }, 0);
+    //   }
+    //   return () => {
+    //     document.removeEventListener('mousedown', handleClickOutside);
+    //   };
+    // }, [isOpen, onClose]);
 
-  if (!isOpen) return null;
+    // 로그아웃 처리
+    const handleClickLogout = () => {
+      console.log('로그아웃 처리');
+      logOut();
+    };
 
-  // 메뉴들 공통 스타일, 마진은 박스 패딩에서 조절했습니다
-  const menuItemClass =
-    'w-[140px] lg:w-[240px] h-[42px] lg:h-[54px] flex items-center px-3 lg:px-6 text-Black-400 hover:bg-Primay-Blue-50 transition-colors';
+    if (!isOpen) return null;
 
-  return (
-    // 감싸는 div에 필요시 z-index 추가해야 할수도 있음 추후?
-    <div
-      ref={dropdownRef}
-      className={clsx(
-        'absolute top-16 right-0 bg-white rounded-xl shadow-md  px-1.5 lg:px-1 pt-2.5 lg:pt-4',
-        'border border-GrayScale-100 overflow-hidden'
-      )}
-    >
-      <div className={clsx(menuItemClass)}>
-        <p className='font-bold text-base lg:text-lg text-Black-400'>
-          {username} {role === 'customer' ? '고객님' : '기사님'}
-        </p>
-      </div>
-      <div className='py-2'>
-        {role === 'customer' ? (
-          // 일반 사용자용 메뉴
-          <>
-            <Link href='/mypage/profile' className={menuItemClass}>
-              프로필 수정
-            </Link>
-            <Link href='/mypage/favorites' className={menuItemClass}>
-              찜한 기사님
-            </Link>
-            <Link href='/mypage/reviews' className={menuItemClass}>
-              이사 리뷰
-            </Link>
-          </>
-        ) : (
-          // 기사님용 메뉴 :('worker')
-          <>
-            <Link href='/worker/profile' className={menuItemClass}>
-              프로필 수정
-            </Link>
-            <Link href='/worker/mypage' className={menuItemClass}>
-              마이페이지
-            </Link>
-            <Link href='/worker/quotes' className={menuItemClass}>
-              받은 견적
-            </Link>
-          </>
+    // 메뉴들 공통 스타일, 마진은 박스 패딩에서 조절했습니다
+    const menuItemClass =
+      'cursor-pointer w-[140px] lg:w-[240px] h-[42px] lg:h-[54px] flex items-center px-3 lg:px-6 text-Black-400 hover:bg-Primay-Blue-50 transition-colors';
+
+    return (
+      // 감싸는 div에 필요시 z-index 추가해야 할 수도 있음
+      <div
+        ref={ref}
+        className={clsx(
+          'absolute top-8 lg:top-13 left-5 lg:left-14 bg-white rounded-xl shadow-md px-1.5 lg:px-1 pt-2.5 lg:pt-4',
+          'border border-GrayScale-100 overflow-hidden z-50'
         )}
-      </div>
+      >
+        <div className={clsx(menuItemClass)}>
+          <p className="font-bold text-base lg:text-lg text-Black-400">
+            {username} {role === 'customer' ? '고객님' : '기사님'}
+          </p>
+        </div>
 
-      <div className='border-t border-GrayScale-100'>
-        <button
-          onClick={handleLogout}
-          className='text-GrayScale-400 text-xs lg:text-sm font-medium w-[140px] lg:w-[240px] h-[38px] lg:h-[45px] hover:bg-Primay-Blue-50 transition-colors'
-        >
-          로그아웃
-        </button>
+        <div className="py-2">
+          {role === 'customer' ? (
+            // 고객용 메뉴
+            <>
+              <Link href="/mypage/profile" className={menuItemClass}>
+                프로필 수정
+              </Link>
+              <div className={menuItemClass} onClick={handleClickFavorite}>
+                찜한 기사님
+              </div>
+              <div className={menuItemClass} onClick={handleClickReview}>
+                이사 리뷰
+              </div>
+            </>
+          ) : (
+            // 기사님용 메뉴
+            <>
+              <Link href="/worker/profile" className={menuItemClass}>
+                프로필 수정
+              </Link>
+              <Link href="/worker/mypage" className={menuItemClass}>
+                마이페이지
+              </Link>
+              <Link href="/worker/quotes" className={menuItemClass}>
+                받은 견적
+              </Link>
+            </>
+          )}
+        </div>
+
+        <div className="border-t border-GrayScale-100">
+          <button
+            onClick={handleClickLogout}
+            className="text-GrayScale-400 text-xs lg:text-sm font-medium w-[140px] lg:w-[240px] h-[38px] lg:h-[45px] hover:bg-Primay-Blue-50 transition-colors"
+          >
+            로그아웃
+          </button>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
 
 export default DropdownProfile;
