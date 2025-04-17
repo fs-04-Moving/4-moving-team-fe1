9164eae5599435ddf2a94cd1e9524506b95b18f8
HJ <a01025105259@gmail.com>
2025-04-17 17:48:53 +0900

    chore: datepicker sm 반응형 구현

--- components/atoms/datepicker/index.tsx
+++ components/atoms/datepicker/index.tsx
@@ -30,7 +30,7 @@ const DatePickerWrapper = ({ onSubmit, selectedDate }: Props) => {
   }, [selectedDate]);
 
   return (
-    <div className="w-[640px] rounded-[32px] bg-white drop-shadow-Chat">
+    <div className="w-[327px] md:w-[640px] rounded-[32px] bg-white drop-shadow-Chat">
       <div className="py-6">
         <div className="flex justify-center">
           <DatePicker
@@ -47,24 +47,33 @@ const DatePickerWrapper = ({ onSubmit, selectedDate }: Props) => {
               prevMonthButtonDisabled,
               nextMonthButtonDisabled,
             }) => (
-              <div className="flex items-center justify-between w-[640px] h-[60px] mb-2">
-                <div className="flex items-center justify-between w-[612px] h-[36px] mx-auto">
+              <div className="w-[327px] h-[48px] md:w-[640px] md:h-[60px] flex items-center justify-between mb-2 mx-auto">
+                <div className="flex items-center justify-between w-full h-[36px]">
                   <button
                     onClick={decreaseMonth}
                     disabled={prevMonthButtonDisabled}
-                    className="w-9 h-9 flex items-center justify-center disabled:opacity-30 cursor-pointer"
+                    className="w-6 h-6 md:w-9 md:h-9 flex items-center justify-center disabled:opacity-30 cursor-pointer"
                   >
-                    <Image src={ChevronLeft} alt="이전" className="w-9 h-9" />
+                    <Image
+                      src={ChevronLeft}
+                      alt="이전"
+                      className="w-6 h-6 md:w-9 md:h-9"
+                    />
                   </button>
-                  <span className="text-[20px] leading-[32px] font-semibold text-[#1F1F1F]">
+
+                  <span className="text-[16px] md:text-[20px] leading-[24px] md:leading-[32px] font-semibold text-[#1F1F1F]">
                     {format(date, 'yyyy. MM')}
                   </span>
                   <button
                     onClick={increaseMonth}
                     disabled={nextMonthButtonDisabled}
-                    className="w-9 h-9 flex items-center justify-center disabled:opacity-30 cursor-pointer"
+                    className="w-6 h-6 md:w-9 md:h-9 flex items-center justify-center disabled:opacity-30 cursor-pointer"
                   >
-                    <Image src={ChevronRight} alt="다음" className="w-9 h-9" />
+                    <Image
+                      src={ChevronRight}
+                      alt="다음"
+                      className="w-6 h-6 md:w-9 md:h-9"
+                    />
                   </button>
                 </div>
               </div>
@@ -72,8 +81,8 @@ const DatePickerWrapper = ({ onSubmit, selectedDate }: Props) => {
           />
         </div>
 
-        <div className="mt-6 flex justify-center">
-          <div className="w-[540px]">
+        <div className="mt-2 md:mt-6 flex justify-center">
+          <div className="w-[279px] h-[54px] md:w-[560px] md:h-[64px]">
             <ButtonSolid disabled={!date} onClick={handleSubmit}>
               선택완료
             </ButtonSolid>
