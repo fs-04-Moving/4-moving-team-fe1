1e7e70a18cc64244e9b05e096533ec2003d570ac
gitit-emkoo <kwcc2020@naver.com>
2025-04-23 16:30:02 +0900

    full merge

--- api/review/writableReview.api.ts
+++ api/review/writableReview.api.ts
@@ -1,43 +1,23 @@
-
-// 🍚데이터 갖고오기 실패하면 예시데이터 사용하기기
-// import { Review } from '@/types/dtos/review.dto';
-// import { client, errorHandler } from '../client'; 
-// import exampleReviewData from './exampleReviewData'; 
-
-// const getReviewableEstimates = async (): Promise<Review[]> => {
-//   const url = '/estimate/reviewable';
-
-//   try {
-//     const response = await client.get(url);
-//     console.log('백엔드에서 리뷰 목록을 성공적으로 가져왔습니다.');
-//     return response.data; 
-
-//   } catch (error) {
-//     console.error('백엔드에서 리뷰 목록을 가져오는 데 실패했습니다.', error)
-//     console.log('백엔드 호출 실패. 예시 데이터를 사용합니다.');
-//     return exampleReviewData;
-//   }
-// };
-
-// const reviewsApi = {
-//   getReviewableEstimates,
-// };
-
-// export default reviewsApi;
-
-
-
 // 🗒️작성한 리뷰 데이터 갖고오는 API
 
-// 기존 데이터 갖고오는 api코드드
-
+// 🍚데이터 갖고오기 실패하면 예시데이터 사용하기기
 import { Review } from '@/types/dtos/review.dto';
-import { client, errorHandler } from '../client';
+import { client, errorHandler } from '../client'; 
+import exampleReviewData from './exampleReviewData'; 
 
 const getReviewableEstimates = async (): Promise<Review[]> => {
   const url = '/estimate/reviewable';
-  const response = await client.get(url);
-  return response.data;
+
+  try {
+    const response = await client.get(url);
+    console.log('백엔드에서 리뷰 목록을 성공적으로 가져왔습니다.');
+    return response.data; 
+
+  } catch (error) {
+    console.error('백엔드에서 리뷰 목록을 가져오는 데 실패했습니다.ㅠㅠㅠ', error)
+    console.log('백엔드 호출 실패. 🍚예시 데이터를 사용합니다.');
+    return exampleReviewData;
+  }
 };
 
 const reviewsApi = {
--- app/(providers)/(root)/customer/reviews/pending/page.tsx
+++ app/(providers)/(root)/customer/reviews/pending/page.tsx
@@ -3,10 +3,25 @@ import writableReview from '@/api/review/writableReview.api';
 import { Review } from '@/types/dtos/review.dto'; 
 import PendingReviewsClient from './PendingReviewsClient'; 
 
-// 메타데이터 OG 넣기기
+// 메타데이터 OG 넣기
 export const metadata: Metadata = {
   title: '리뷰 작성하기',
   description: '작성 가능한 이사 서비스 리뷰 목록입니다.',
+  openGraph: {
+    title: '이사할땐, 무빙',
+    description: '작성 가능한 이사 서비스 리뷰 목록입니다.',
+    //url: '현재 URL', 
+    siteName: '이사할땐, 무빙', 
+    type: 'website',
+    images: [
+      {
+        url: 'https://m.luxblock.co.kr/file_data/luxblook/2020/08/17/4b0708ca352f2f903ed0ef0162bac4f2.png',
+        width: 1200,
+        height: 630,
+        alt: '리뷰 작성하기 페이지 미리보기 이미지',
+      },
+    ],
+  },
 };
 
 async function getPendingReviews(): Promise<Review[]> {
