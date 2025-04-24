ac0609fe1d364f39c6bafec6928e503616684585
Jinho Jeong <zinojung@users.noreply.github.com>
2025-04-24 16:35:34 +0900

    feat: 고객 기본정보 수정 구현

--- app/(providers)/(root)/customer/info/edit/page.tsx
+++ app/(providers)/(root)/customer/info/edit/page.tsx
@@ -1,19 +1,40 @@
 'use client';
 
+import userApi from '@/api/user/user.api';
+import LoadingSpinner from '@/components/atoms/LoadingSpinner';
 import CustomerInfoEditTemplate from '@/components/templates/CustomerInfoEditTemplate';
+import { useCustomerInfoQuery } from '@/hooks/useCustomerInfoQuery';
+import { UpdateUserInfoDto } from '@/types/dtos/user.dto';
+import { useMutation } from '@tanstack/react-query';
 
 function CustomerInfoEditPage() {
-  const defaultValues = {
-    name: '',
-    email: '',
-    phone: '',
-    currentPassword: '',
+  const { data, isLoading, isError } = useCustomerInfoQuery();
+
+  const mutation = useMutation({
+    mutationFn: userApi.updateUserInfo,
+    onSuccess: () => {
+      alert('정보가 성공적으로 수정되었습니다.');
+    },
+    onError: () => {
+      alert('정보 수정에 실패했습니다. 비밀번호를 확인해 주세요.');
+    },
+  });
+
+  if (isLoading) return <LoadingSpinner />;
+  if (isError) return <div>데이터를 불러올 수 없습니다.</div>;
+
+  const defaultValues: UpdateUserInfoDto = {
+    name: data.name,
+    email: data.email,
+    phoneNumber: data.phoneNumber,
+    password: '',
     newPassword: '',
-    newPasswordConfirmed: '',
+    newPasswordConfirm: '',
   };
 
-  const onSubmit = (data: object) => {
-    console.log(data);
+  const onSubmit = (formData: UpdateUserInfoDto) => {
+    console.log('formdData in onSubmit', formData);
+    mutation.mutate(formData);
   };
 
   return (
--- components/templates/CustomerInfoEditTemplate.tsx
+++ components/templates/CustomerInfoEditTemplate.tsx
@@ -6,19 +6,46 @@ import ButtonSolid from '@/components/atoms/ButtonSolid';
 import InputEmail from '@/components/molecules/InputEmail';
 import InputPassword from '@/components/molecules/InputPassword';
 import InputText from '@/components/molecules/InputText';
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { UpdateUserInfoDto } from '@/types/dtos/user.dto';
 
 interface CustomerInfoEditTemplateProps {
-  defaultValues: {
-    name: string;
-    email: string;
-    phone: string;
-    currentPassword: string;
-    newPassword: string;
-    newPasswordConfirmed: string;
-  };
-  onSubmit: (data: object) => void;
+  defaultValues: UpdateUserInfoDto;
+  onSubmit: SubmitHandler<UpdateUserInfoDto>;
 }
+
+/**
+ * CustomerInfoEditTemplate
+ *
+ * 사용자 정보 수정 페이지에 공통으로 사용할 수 있는 템플릿 컴포넌트입니다.
+ * 이름, 이메일, 전화번호, 비밀번호 수정 등의 항목을 포함하며,
+ * `defaultValues`와 `onSubmit` 핸들러를 통해 유연하게 재사용할 수 있습니다.
+ *
+ * @component
+ * @example
+ * ```tsx
+ * <CustomerInfoEditTemplate
+ *   defaultValues={{
+ *     name: '홍길동',
+ *     email: 'hong@example.com',
+ *     phoneNumber: '010-1234-5678',
+ *     password: '',
+ *     newPassword: '',
+ *     newPasswordConfirm: '',
+ *   }}
+ *   onSubmit={(data) => console.log(data)}
+ * />
+ * ```
+ *
+ * @param {Object} defaultValues - 각 입력 항목의 기본값을 설정
+ * @param {string} defaultValues.name - 사용자 이름
+ * @param {string} defaultValues.email - 사용자 이메일 (disabled 상태)
+ * @param {string} defaultValues.phoneNumber - 사용자 전화번호
+ * @param {string} defaultValues.password - 현재 비밀번호
+ * @param {string} defaultValues.newPassword - 새 비밀번호
+ * @param {string} defaultValues.newPasswordConfirm - 새 비밀번호 확인
+ * @param {SubmitHandler<UpdateUserInfoDto>} onSubmit - 폼 제출 시 호출될 핸들러
+ */
 
 function CustomerInfoEditTemplate({
   defaultValues,
@@ -47,12 +74,13 @@ function CustomerInfoEditTemplate({
                 name="email"
                 control={control}
                 bgColor={true}
-                disabled
+                readOnly
+                className="bg-BackGround-200 text-gray-300 w-full rounded-2xl text-[16px] lg:text-xl p-3.5 md:h-[54px] lg:h-[64px]"
               />
               <hr className="border-[1px] border-Line-100" />
               <InputText
                 label="전화번호"
-                name="phone"
+                name="phoneNumber"
                 control={control}
                 bgColor={true}
                 placeholder="전화번호를 입력해 주세요"
@@ -62,7 +90,7 @@ function CustomerInfoEditTemplate({
             <div className="flex flex-col gap-8 w-[327px] lg:w-[640px]">
               <InputPassword
                 label="현재 비밀번호"
-                name="currentPassword"
+                name="password"
                 control={control}
                 bgColor={true}
                 placeholder={'현재 비밀번호를 입력해주세요'}
@@ -78,7 +106,7 @@ function CustomerInfoEditTemplate({
               <hr className="border-[1px] border-Line-100" />
               <InputPassword
                 label="새 비밀번호 확인"
-                name="newPasswordConfirmed"
+                name="newPasswordConfirm"
                 control={control}
                 bgColor={true}
                 placeholder={'새 비밀번호를 다시 한번 입력해주세요'}
