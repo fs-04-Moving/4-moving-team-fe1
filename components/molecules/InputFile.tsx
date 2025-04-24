384e8ffa28743578c49c16b8301d79249cd493fa
Hyeongmin Jo <paengdal1@gmail.com>
2025-04-24 14:41:24 +0900

    feat: 이미지 업로드 validation 추가 - 용량: 1MB이하로 제한 - 규격: PNG, JPEG, JPG, GIF

--- components/molecules/InputFile.tsx
+++ components/molecules/InputFile.tsx
@@ -1,4 +1,5 @@
 import icGallery from '@/assets/images/ic-gallery.svg';
+import { ALLOWED_TYPES, MAX_FILE_SIZE, MAX_FILE_SIZE_MB } from '@/constants';
 import Image from 'next/image';
 import React, { ChangeEventHandler, useEffect, useRef, useState } from 'react';
 import {
@@ -50,6 +51,22 @@ function InputFile<
     const { files } = e.target as HTMLInputElement;
     if (!files || files.length === 0) return;
     const file = files[0];
+
+    if (!ALLOWED_TYPES.includes(file.type)) {
+      alert('JPG 또는 PNG, GIF 형식만 업로드 가능합니다.');
+      fileInputRef.current!.value = '';
+      return;
+    }
+
+    if (file.size > MAX_FILE_SIZE) {
+      alert(`파일 용량은 ${MAX_FILE_SIZE_MB}MB 이하만 가능합니다.`);
+      // input 값 리셋 (같은 파일 다시 선택 가능하도록)
+      if (fileInputRef.current) {
+        fileInputRef.current.value = '';
+      }
+      return;
+    }
+
     onChange(file);
     const fileUrl = URL.createObjectURL(file);
     setPickedFileUrl(fileUrl);
