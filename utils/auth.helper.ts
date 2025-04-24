684a3fc1bdc3d9c11707cd9bb5dd23c398d88f26
Hyeongmin Jo <paengdal1@gmail.com>
2025-04-24 11:47:21 +0900

    feat: logoutHelper 생성 - 목적: axios interceptor에서 리프리시 토큰 발급 실패시 로그아웃 실행을 위해   - client.ts와 authcontext에 해당 함수 사용 - 참고: useAuth를 사용하지 못하는 이유   - interceptor는 React Context를 직접 사용할 수가 없다

---
+++ utils/auth.helper.ts
@@ -0,0 +1,27 @@
+'use client'; // App Router 환경에서 클라이언트 전용으로 선언
+
+import authApi from '@/api/auth/auth.api';
+import { client } from '@/api/client';
+import { getBrowserQueryClient } from '@/libs/tanstack-query/reactQueryConfig';
+
+/**
+ * 전역 로그아웃 로직 (Interceptor 등 React Context 밖에서 사용)
+ * @param onAfterLogout - 로그아웃 후 실행할 콜백 (라우팅 등)
+ */
+export const logoutHelper = async (onAfterLogout?: () => void) => {
+  try {
+    await authApi.logOut();
+  } catch (e) {
+    console.error('서버 로그아웃 실패', e);
+  }
+
+  client.defaults.headers['Authorization'] = '';
+  localStorage.removeItem('accessToken');
+
+  const queryClient = getBrowserQueryClient();
+  queryClient.removeQueries({ queryKey: ['me'] });
+
+  if (onAfterLogout) {
+    onAfterLogout();
+  }
+};
