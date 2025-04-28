056d8ed9f77a8f6c23f46534e38e9af210c697a9
gitit-emkoo <kwcc2020@naver.com>
2025-04-28 14:03:52 +0900

    feat:데이터리스트 없을때 보여주는 컴포넌트 구현 - 리뷰없을때 컴포넌트 재사용

--- components/molecules/EmptyWritableReview.tsx
+++ components/molecules/EmptyWritableReview.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import ImgEmptyReview from '@/assets/images/img-empty-review.svg';
 import Image from 'next/image';
 
-function EmptyReview() {
+interface EmptyDataListProps {
+  text: string;
+}
+
+function EmptyDataList({ text }: EmptyDataListProps) {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="w-[253px] h-[200px] flex flex-col items-center justify-center">
-        <Image src={ImgEmptyReview} alt="리뷰없음" />
-        <h1 className="mt-4 text-center">작성 가능한 리뷰가 없어요</h1>
+        <Image src={ImgEmptyReview} alt="목록 없음" />
+        <h1 className="mt-4 text-center">{text}</h1>
       </div>
     </div>
-  );
+  )
 }
 
-export default EmptyReview;
+export default EmptyDataList;
