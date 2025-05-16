7538ed3190c1dccde750a5ba231944e06bc71486
paengdal <40021741+paengdal@users.noreply.github.com>
2025-05-16 17:39:19 +0900

    [조형민] 메인페이지 텍스트 수정, 완료 모달 추가 (#262)
    * feat: 프로필 작성(수정) 및 견적 요청 완료 시 모달 팝업
    
    * chore: 메인페이지 오타 수정
    - 가정이사 2개 중복

--- utils/handlerUtils.ts
+++ utils/handlerUtils.ts
@@ -1,6 +1,7 @@
 import { User } from '@/types/entities/user.entity';
 import { QueryClient } from '@tanstack/react-query';
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import Swal from 'sweetalert2';
 
 /**
  * 프로필 작성 완료 후의 공통 처리
@@ -18,5 +19,14 @@ export async function handleProfileSuccess({
   const user: User | undefined = queryClient.getQueryData(['me']);
 
   if (!user) return;
-  router.push(`/${user.role}`);
+  Swal.fire({
+    text: '완료되었습니다.',
+    icon: 'info',
+    confirmButtonText: '확인 ',
+    confirmButtonColor: '#3085d6',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      router.push(`/${user.role}`);
+    }
+  });
 }
